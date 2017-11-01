9e6017edd48c34974072817ce2e8fc0f2a48b346
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-11-01 22:28:11 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -5,7 +5,11 @@ class Patient
   def initialize(name)
     @name = name
     @appointments = []
+  end
 
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.patient = self
   end
 
 end
